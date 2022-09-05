10131b763f150cae376e4b1d0a875953c7627133
ComRV <r.duika.9d@gmail.com>
2022-09-05 17:22:54 +0700

    setup laravel and tailwindcss

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="">
+        <div class="p-4 mb-4 w-1/4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800">
+            testing
+        </div>
+    </div>
+</body>
+
+</html>
